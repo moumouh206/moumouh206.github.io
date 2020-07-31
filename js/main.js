5a627209575efd88bc52816a23ea317566c94ef3
BIDA Mohamed Amine <moumouh206@gmail.com>
2020-07-31 17:11:40 +0100

    initinit

--- js/main.js
+++ js/main.js
@@ -1,157 +1,176 @@
-/*global $, jQuery, alert*/
-$(document).ready(function() {
+/* Table of Contents
+==================================================
+# Navigation Height
+# Counter
+# Progress Bar
+# EasyPieChart
+# MagnificPopup
+# onePageNav
+# Sticky Nav
+# Mobile Toggle Control
 
-  'use strict';
+==================================================
+*/
 
-  // ========================================================================= //
-  //  //SMOOTH SCROLL
-  // ========================================================================= //
+// -------------------------------------------------------------
+//  Navigation Height 
+// -------------------------------------------------------------
 
+(function() {
 
-  $(document).on("scroll", onScroll);
+    var height = $(window).height();
+     $(".menu-one .navbar-nav").innerHeight(height);
+    
+}());    
+           
+// -------------------------------------------------------------
+//Mobile Toggle Control
+// -------------------------------------------------------------
 
-  $('a[href^="#"]').on('click', function(e) {
-    e.preventDefault();
-    $(document).off("scroll");
+menuHide();
+function menuHide(){
+    var windowWidth = $(window).width();
+        if(windowWidth < 991 ){
+            $('.navbar-nav a').click(function (e) {
+            $('.navbar-collapse').collapse('toggle');
+        });
+    }  
+}    
+          
+// -------------------------------------------------------------
+// Counter
+// -------------------------------------------------------------
 
-    $('a').each(function() {
-      $(this).removeClass('active');
-      if ($(window).width() < 768) {
-        $('.nav-menu').slideUp();
-      }
+(function () {
+
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    
     });
 
-    $(this).addClass('active');
+}());
 
-    var target = this.hash,
-        menu = target;
+// -------------------------------------------------------------
+// Progress Bar
+// -------------------------------------------------------------
 
-    target = $(target);
-    $('html, body').stop().animate({
-      'scrollTop': target.offset().top - 80
-    }, 500, 'swing', function() {
-      window.location.hash = target.selector;
-      $(document).on("scroll", onScroll);
-    });
-  });
+(function () {
 
-
-  function onScroll(event) {
-    if ($('.home').length) {
-      var scrollPos = $(document).scrollTop();
-      $('nav ul li a').each(function() {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-      });
-    }
-  }
-
-  // ========================================================================= //
-  //  //NAVBAR SHOW - HIDE
-  // ========================================================================= //
-
-
-  $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    if (scroll > 200 ) {
-      $("#main-nav, #main-nav-subpage").slideDown(700);
-      $("#main-nav-subpage").removeClass('subpage-nav');
-    } else {
-      $("#main-nav").slideUp(700);
-      $("#main-nav-subpage").hide();
-      $("#main-nav-subpage").addClass('subpage-nav');
-    }
-  });
-
-  // ========================================================================= //
-  //  // RESPONSIVE MENU
-  // ========================================================================= //
-
-  $('.responsive').on('click', function(e) {
-    $('.nav-menu').slideToggle();
-  });
-
-  // ========================================================================= //
-  //  Typed Js
-  // ========================================================================= //
-
-  var typed = $(".typed");
-
-  $(function() {
-    typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
-      typeSpeed: 100,
-      loop: true,
-    });
-  });
-
-
-  // ========================================================================= //
-  //  Owl Carousel Services
-  // ========================================================================= //
-
-
-  $('.services-carousel').owlCarousel({
-      autoplay: true,
-      loop: true,
-      margin: 20,
-      dots: true,
-      nav: false,
-      responsiveClass: true,
-      responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 4 } }
-    });
-
-  // ========================================================================= //
-  //  magnificPopup
-  // ========================================================================= //
-
-  var magnifPopup = function() {
-    $('.popup-img').magnificPopup({
-      type: 'image',
-      removalDelay: 300,
-      mainClass: 'mfp-with-zoom',
-      gallery: {
-        enabled: true
-      },
-      zoom: {
-        enabled: true, // By default it's false, so don't forget to enable it
-
-        duration: 300, // duration of the effect, in milliseconds
-        easing: 'ease-in-out', // CSS transition easing function
-
-        // The "opener" function should return the element from which popup will be zoomed in
-        // and to which popup will be scaled down
-        // By defailt it looks for an image tag:
-        opener: function(openerElement) {
-          // openerElement is the element on which popup was initialized, in this case its <a> tag
-          // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-          return openerElement.is('img') ? openerElement : openerElement.find('img');
+    $('.progress-content').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $.each($('div.progress-bar'),function(){
+                $(this).css('width', $(this).attr('aria-valuenow')+'%');
+            });
+            $(this).unbind('inview');
         }
+    });
+    $('.rating-bar').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $.each($('div.progress-bar'),function(){
+                $(this).css('width', $(this).attr('aria-valuenow')+'%');
+            });
+            $(this).unbind('inview');
+        }
+    });
+
+}());
+
+
+// -------------------------------------------------------------
+// EasyPieChart
+// -------------------------------------------------------------
+
+(function () {
+
+    $('.language-skill').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $('.chart').easyPieChart({
+                //your configuration goes here
+                easing: 'easeOut',
+                delay: 3000,
+                scaleColor: false,
+                animate: 2000,
+                onStep: function(from, to, percent) {
+                    this.el.children[0].innerHTML = Math.round(percent);
+                }
+
+            });
+        }
+    }); 
+}());
+
+
+// -------------------------------------------------------------
+// MagnificPopup
+// -------------------------------------------------------------
+
+(function () {
+    $('.portfolio-info a').magnificPopup({
+      type: 'image',
+      gallery:{
+        enabled:true
       }
     });
-  };
+}());
 
+// -------------------------------------------------------------
+// Navigation Scroll
+// -------------------------------------------------------------
 
-  // Call the functions
-  magnifPopup();
+$(window).scroll(function(event) {
+    Scroll();
+}); 
 
+$('.navbar-nav li a').click(function() {  
+    $('html, body').animate({scrollTop: $(this.hash).offset().top -1}, 1000);
+    return false;
 });
 
-// ========================================================================= //
-//  Porfolio isotope and filter
-// ========================================================================= //
-$(window).load(function(){
+// User define function
+function Scroll() {
+    var contentTop      =   [];
+    var contentBottom   =   [];
+    var winTop      =   $(window).scrollTop();
+    var rangeTop    =   200;
+    var rangeBottom =   500;
+    $('.navbar-nav').find('.scroll a').each(function(){
+        contentTop.push( $( $(this).attr('href') ).offset().top);
+        contentBottom.push( $( $(this).attr('href') ).offset().top + $( $(this).attr('href') ).height() );
+    })
+    $.each( contentTop, function(i){
+        if ( winTop > contentTop[i] - rangeTop ){
+            $('.navbar-nav li.scroll')
+            .removeClass('current')
+            .eq(i).addClass('current');          
+        }
+    })
 
-  var portfolioIsotope = $('.portfolio-container').isotope({
-    itemSelector: '.portfolio-thumbnail',
-    layoutMode: 'fitRows'
-  });
+};
+    
+// -------------------------------------------------------------
+//  Sticky Nav
+// -------------------------------------------------------------
+(function () {  
+    function menuToggle(){
+        var windowWidth = $(window).width();
+        if(windowWidth > 991 ){
+            $(window).on('scroll', function(){
+                if( $(window).scrollTop()>735 ){
+                    $('.home-two .navbar').addClass('fixed-top animated fadeInDown');
+                } else {
+                    $('.home-two .navbar').removeClass('fixed-top animated fadeInDown');
+                };
+            });
+        }else{
+            
+            $('.home-two .navbar').addClass('fixed-top');
+                
+        };  
+    }
 
-  $('#portfolio-flters li').on( 'click', function() {
-    $("#portfolio-flters li").removeClass('filter-active');
-    $(this).addClass('filter-active');
+    menuToggle();
+}()); 
 
-    portfolioIsotope.isotope({ filter: $(this).data('filter') });
-  });
 
-})
+
